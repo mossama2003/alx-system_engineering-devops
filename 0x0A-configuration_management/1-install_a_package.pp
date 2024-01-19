4ddbb5510aced02768085980e8a7e5c66e837073
mossamaa2003 <mossama370@gmail.com>
2024-01-19 11:51:13 -0800

    Server

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,19 @@
 #!/usr/bin/pup
 # Install python3-pip, flask 2.1.0, and werkzeug 2.1.1.
 
-package { 
-	'python3-pip':
-	ensure => present;
+package {
+  'python3-pip':
+    ensure => present;
 }
 
 exec { 'pip3 install flask==2.1.0':
-	path => '/usr/bin',
-	unless => 'pip3 show flask | grep Version | grep 2.1.0',
-	require => Package['python3-pip'];
+  path    => '/usr/bin',
+  unless  => 'pip3 show flask | grep Version | grep 2.1.0',
+  require => Package['python3-pip'];
 }
 
 exec { 'pip3 install werkzeug==2.1.1':
-	path => '/usr/bin',
-	unless => 'pip3 show werkzeug | grep Version | grep 2.1.1',
-	require => Package['python3-pip'];
+  path    => '/usr/bin',
+  unless  => 'pip3 show werkzeug | grep Version | grep 2.1.1',
+  require => Package['python3-pip'];
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,7 @@
-# execute pkill command to kill bashscript file killmenow
+# kill proccess killmenow
+
 exec { 'pkill':
   command => 'pkill -9 -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
+  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
+  onlyif  => 'pgrep -f killmenow',
 }
