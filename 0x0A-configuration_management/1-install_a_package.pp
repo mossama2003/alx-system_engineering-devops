b27dae65917e50b947f55d9ae5c6d6ad0f43fd5f
mossamaa2003 <mossama370@gmail.com>
2024-01-19 10:31:11 -0800

    Server

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Ensure python3-pip is installed
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require => package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure => '2.0.1',
+  provider => 'pip',
+  require => package['python3-pip'],
 }
