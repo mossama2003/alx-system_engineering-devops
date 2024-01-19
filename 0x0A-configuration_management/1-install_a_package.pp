197cd749fa30287d1549dba2c655e9155c3d4143
mossamaa2003 <mossama370@gmail.com>
2024-01-19 11:33:19 -0800

    Server

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,19 @@
 #!/usr/bin/pup
-# Ensure python3-pip is installed
+# Install python3-pip, flask 2.1.0, and werkzeug 2.1.1.
 
-package { 'python3-pip':
-  ensure => installed,
+package { 
+	'python3-pip':
+	ensure => present;
 }
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
-  require => package['python3-pip'],
+exe { 'pip3 install flask==2.1.0':
+	path => '/usr/bin',
+	unless => 'pip3 show flask | grep Version | grep 2.1.0',
+	require => package['python3-pip'];
 }
 
-package { 'werkzeug':
-  ensure => '2.0.1',
-  provider => 'pip',
-  require => package['python3-pip'],
+exe { 'pip3 install werkzeug==2.1.1':
+	path => '/usr/bin',
+	unless => 'pip3 show werkzeug | grep Version | grep 2.1.1',
+	require => package['python3-pip'],
 }
