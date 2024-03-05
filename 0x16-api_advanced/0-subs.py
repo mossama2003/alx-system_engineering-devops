af2c03c74d750449512f1a5c450e2c5209197eb9
mossamaa2003 <mossama370@gmail.com>
2024-03-05 14:43:22 -0800

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json" .format(subreddit)
     headers = {
             "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov)"
-            }
+    ~}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
