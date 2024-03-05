70fcb156fbfd8855044da3b04a681daaa1b6f822
mossamaa2003 <mossama370@gmail.com>
2024-03-05 15:28:46 -0800

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,17 +4,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-<<<<<<< HEAD
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json" .format(subreddit)
-    headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov)"
-    ~}
-=======
     """Reddit API endpoint for subreddit information"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'u/nourhan1414'}
->>>>>>> 050465579d663be54d19d5b8aa0a561df35fab4a
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         return 0
