a2352b6f09303762abcad93790f59d0df62ecad0
Mohamed Osama <mossama370@gmail.com>
2024-03-06 23:51:45 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Reddit API endpoint for subreddit information"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'u/nourhan1414'}
+    """print the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "u/Responsible_Peace_80"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    if response.status_code >= 300:
         return 0
-    data = response.json().get("data")
-    subscribers = data.get('subscribers')
-    return subscribers
+    return response.json().get("data").get("subscribers")
